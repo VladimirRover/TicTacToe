f4a8687b996973302e9115b6847ffea5ee64353c
Vladimir Rover <opttps@mail.ru>
2017-04-16 16:17:36 +0300

    omg

--- src/Field.java
+++ src/Field.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by User on 16.04.2017.
  */
-public class Filed {
+public class Field {
+
+    private JButton[] button;
+    public void buildGUI(){
+        JFrame frame = new JFrame("Tic - Tac -Toe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 300, 300);
+
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 3));
+
+        button = new JButton[9];
+        for (int i = 0; i < button.length; i++) {
+            button[i] = new JButton("");
+            panel.add(button[i]);
+        }
+
+        frame.add(BorderLayout.CENTER, panel);
+        frame.setVisible(true);
+    }
 }
